b6f3097526422e0a962b6a4dc8ad3c36b51e74d6
sunnywind <sunny-wind@foxmail.com>
2023-04-15 12:01:43 +0800

    init

---
+++ build/functions/tencent.qrcode.js
@@ -0,0 +1,33 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.QrCodeScan = void 0;
+const tencentcloud = require('tencentcloud-sdk-nodejs');
+const OcrClient = tencentcloud.ocr.v20181119.Client;
+const file_1 = require("../utils/file");
+// const ENVJSON = (0, file_1.getJsonObject)('env.json')||null;
+const QrCodeScan = async (source, type) => {
+    // let result;
+    // const payload = {};
+    // const client = new OcrClient({
+    //     credential: {
+    //         secretId: ENVJSON.tencent.secretId,
+    //         secretKey: ENVJSON.tencent.secretKey,
+    //     },
+    //     region: 'ap-shanghai',
+    //     profile: {
+    //         httpProfile: {
+    //             endpoint: 'ocr.tencentcloudapi.com',
+    //         },
+    //     },
+    // });
+    // type === 'base64' ? payload.ImageBase64 = source : payload.ImageUrl = source;
+    // try {
+    //     result = await client.QrcodeOCR(payload);
+    // }
+    // catch (err) {
+    //     result = '';
+    // }
+    // return result;
+    return ''
+};
+exports.QrCodeScan = QrCodeScan;
